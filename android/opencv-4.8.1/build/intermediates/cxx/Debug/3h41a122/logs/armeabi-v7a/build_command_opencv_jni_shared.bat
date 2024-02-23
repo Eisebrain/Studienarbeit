3e9799837edb5f70e7789eaffb230f31e130a616
TimoFeucht <timo@feucht-web.de>
2024-02-23 09:25:35 +0100

    Removed opencv-4.8.1/build vom vcs

--- android/opencv-4.8.1/build/intermediates/cxx/Debug/3h41a122/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Timo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Timo\\AndroidStudioProjects\\Studienarbeit-Android\\android\\opencv-4.8.1\\.cxx\\Debug\\3h41a122\\armeabi-v7a" ^
-  opencv_jni_shared
