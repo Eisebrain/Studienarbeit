5f57134464eac472a96de99a903c158ca0e3880e
TimoFeucht <timo@feucht-web.de>
2024-05-16 18:55:07 +0200

    feat: detailed LSit analysis added

--- android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/exercises/LSit.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/exercises/LSit.kt
@@ -8,10 +8,18 @@ class LSit {
         private const val THRESHOLD_MIN = 173
         private const val THRESHOLD_PERFECT = 176
         private const val THRESHOLD_MAX = 182
+        private const val THRESHOLD_HIP = 95
+
+        // private const val THRESHOLD_DIFF = 10
         val exerciseUtils = ExerciseUtils()
     }
 
-    fun isLSit(person: Person): Boolean {
+    /**
+     * Check if the person is doing an L-Sit.
+     * @param person Person to check.
+     * @return 0 if not an L-Sit, 1 if an L-Sit but not perfect, 2 if a perfect L-Sit.
+     */
+    fun isLSit(person: Person): Int {
         val leftKnee = exerciseUtils.extractKeypoint(person, BodyPart.LEFT_KNEE)
         val rightKnee = exerciseUtils.extractKeypoint(person, BodyPart.RIGHT_KNEE)
         val leftHip = exerciseUtils.extractKeypoint(person, BodyPart.LEFT_HIP)
@@ -25,17 +33,40 @@ class LSit {
         val leftWrist = exerciseUtils.extractKeypoint(person, BodyPart.LEFT_WRIST)
         val rightWrist = exerciseUtils.extractKeypoint(person, BodyPart.RIGHT_WRIST)
 
-        val angleLeftKnee = exerciseUtils.calculateAngleBetweenThreePoints(leftHip, leftKnee, leftAnkle)
-        val angleRightKnee = exerciseUtils.calculateAngleBetweenThreePoints(rightHip, rightKnee, rightAnkle)
+        val angleLeftKnee =
+            exerciseUtils.calculateAngleBetweenThreePoints(leftHip, leftKnee, leftAnkle)
+        val angleRightKnee =
+            exerciseUtils.calculateAngleBetweenThreePoints(rightHip, rightKnee, rightAnkle)
         //println("Right knee angle: $angleRightKnee")
-        val angleLeftElbow = exerciseUtils.calculateAngleBetweenThreePoints(leftShoulder, leftElbow, leftWrist)
-        val angleRightElbow = exerciseUtils.calculateAngleBetweenThreePoints(rightShoulder, rightElbow, rightWrist)
+        val angleLeftElbow =
+            exerciseUtils.calculateAngleBetweenThreePoints(leftShoulder, leftElbow, leftWrist)
+        val angleRightElbow =
+            exerciseUtils.calculateAngleBetweenThreePoints(rightShoulder, rightElbow, rightWrist)
         //println("Right elbow angle: $angleRightElbow")
+        val angleLeftHip =
+            exerciseUtils.calculateAngleBetweenThreePoints(leftShoulder, leftHip, leftKnee)
+        val angleRightHip =
+            exerciseUtils.calculateAngleBetweenThreePoints(rightShoulder, rightHip, rightKnee)
+        //println("Hip angle: angleLeftHip")
 
-        // ToDo: return int (eg. 0 for not detected, 1 for detected but not correct, 2 for correct)
-        return (THRESHOLD_MIN < angleLeftKnee && angleLeftKnee < THRESHOLD_MAX &&
-                THRESHOLD_MIN < angleRightKnee && angleRightKnee < THRESHOLD_MAX) ||
-                (THRESHOLD_MIN < angleLeftElbow && angleLeftElbow < THRESHOLD_MAX &&
+        if (angleLeftHip < THRESHOLD_HIP || angleRightHip < THRESHOLD_HIP) {
+            // something like a LSit
+            if ((THRESHOLD_PERFECT < angleLeftKnee && angleLeftKnee < THRESHOLD_MAX &&
+                THRESHOLD_PERFECT < angleLeftElbow && angleLeftElbow < THRESHOLD_MAX) ||
+                (THRESHOLD_PERFECT < angleRightKnee && angleRightKnee < THRESHOLD_MAX &&
+                THRESHOLD_PERFECT < angleRightElbow && angleRightElbow < THRESHOLD_MAX)
+            ) {
+                // perfect LSit
+                return 2
+            } else if ((THRESHOLD_MIN < angleLeftKnee && angleLeftKnee < THRESHOLD_MAX &&
+                THRESHOLD_MIN < angleLeftElbow && angleLeftElbow < THRESHOLD_MAX) ||
+                (THRESHOLD_MIN < angleRightKnee && angleRightKnee < THRESHOLD_MAX &&
                 THRESHOLD_MIN < angleRightElbow && angleRightElbow < THRESHOLD_MAX)
+            ) {
+                // not perfect LSit, but still LSit
+                return 1
+            }
+        }
+        return 0
     }
 }
